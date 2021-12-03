e41f555ea35d94a6e0f802194d7f92ab30e85271
CovidT <95062356+CovidT@users.noreply.github.com>
2021-12-02 20:12:27 -0500

    Add files via upload

---
+++ models/Government.py
@@ -0,0 +1,13 @@
+
+class Government:
+    username = 'null'
+    password = 'null'
+
+    def getUsername(self):
+        return self.username
+
+    def getPassword(self):
+        return self.password
+
+
+
