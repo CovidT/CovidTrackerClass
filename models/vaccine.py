755b2811e565ed08183cdee153d8a5f40813e694
CovidT <95062356+CovidT@users.noreply.github.com>
2021-12-02 20:11:30 -0500

    Create vaccine.py

---
+++ models/vaccine.py
@@ -0,0 +1,79 @@
+
+class VaccinationDetails:
+    name = 'null'
+    dateofbirth = 'null'
+    email = 'null'
+    location = 'null'
+    uniqueid = 'null'
+    date = 'null'
+    firstdose = 'null'
+    firstname = 'null'
+    seconddose = 'null'
+    secondname = 'null'
+    seconddate = 'null'
+
+    def getname(self):
+        return self.name
+
+    def getdateofbirth(self):
+        return self.dateofbirth
+
+    def getemail(self):
+        return self.email
+
+    def getlocation(self):
+        return self.location
+
+    def getuniqueid(self):
+        return self.uniqueid
+
+    def getdate(self):
+        return self.date
+
+    def getfirstdose(self):
+        return self.firstdose
+
+    def getfirstname(self):
+        return self.firstname
+
+    def getseconddose(self):
+        return self.seconddose
+
+    def getsecondname(self):
+        return self.secondname
+
+    def getseconddate(self):
+        return self.seconddate
+
+    def setname(self,name):
+         self.name = name
+
+    def setdateofbirth(self,dateofbirth):
+         self.dateofbirth = dateofbirth
+
+    def setemail(self,email):
+         self.email =email
+
+    def setlocation(self,location):
+        self.location = location
+
+    def setuniqueid(self,uniqueid):
+         self.uniqueid = uniqueid
+
+    def setdate(self,date):
+         self.date = date
+
+    def setfirstdose(self,firstdose):
+        self.firstdose = firstdose
+
+    def setfirstname(self,firstname):
+        self.firstname = firstname
+
+    def setseconddose(self,seconddose):
+        self.seconddose = seconddose
+
+    def setsecondname(self,secondname):
+        self.secondname = secondname
+
+    def setseconddate(self,seconddate):
+        self.seconddate = seconddate
